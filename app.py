107d495c629052380960731a975911cdd5966ec9
Fitzpatrique <rickogbonna@gmail.com>
2022-12-02 19:28:52 +0100

    Integrated Chatbot

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from config import *
 from flask import *
+from chat import get_response
 import json
 import string
 import random
@@ -9,7 +10,8 @@ email = 'fitzogbonna@gmail.com'
 phone = '+2348032308094'
 amount = '5000'
 
-app = Flask(__name__) 
+app = Flask(__name__)
+app.config['SECRET'] = "Oy!sWestAfr!ca03%"
 
 @app.route('/', methods=['POST','GET'])
 @app.route('/index', methods=['POST','GET'])
@@ -36,17 +38,33 @@ def cart():
     data_function()
     return render_template('cart.html')
 
+
 @app.route('/checkout', methods=['POST','GET'])
 def checkout():
     return render_template('checkout.html')
+
+@app.route('/<email>/profile', methods=['POST','GET'])
+def customers_profile():
+    return render_template('customers_profile.html')
+
+@app.route('/static/data.json', methods=['POST','GET'])
+def data():
+    return app.send_static_file('data.json')
 
 @app.route('/confirmation_successful', methods=['POST','GET'])
 def payment():
     return render_template('confirmation_successful.html')
 
-@app.route('/static/data.json', methods=['POST','GET'])
-def data():
-    return app.send_static_file('data.json')
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer":response}
+    return jsonify(message)
+
+@app.route('/sign_in', methods=['POST','GET'])
+def sign_in():
+    return render_template('sign_in.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
---
+++ database.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+#Open database
+conn = sqlite3.connect('database.db')
+
+#Create table
+conn.execute('''CREATE TABLE users 
+		(userId INTEGER PRIMARY KEY, 
+		password TEXT,
+		email TEXT,
+		username TEXT,
+		created_at TEXT,
+		delivery_address TEXT,
+		subscription_status TEXT,
+		city TEXT,
+		state TEXT,
+		country TEXT, 
+		phone TEXT
+		)''')
+
+conn.execute('''CREATE TABLE products
+		(productId INTEGER PRIMARY KEY,
+		name TEXT,
+		price INTEGER,
+        compared_price INTEGER,
+        vendor TEXT,
+        tags TEXT,
+        variants,
+		description TEXT,
+		image TEXT,
+		stock INTEGER,
+		categoryId INTEGER,
+		FOREIGN KEY(categoryId) REFERENCES categories(categoryId)
+		)''')
+
+conn.execute('''CREATE TABLE kart
+		(userId INTEGER,
+		productId INTEGER,
+        amount INTEGER,
+		FOREIGN KEY(userId) REFERENCES users(userId),
+		FOREIGN KEY(productId) REFERENCES products(productId)
+		)''')
+
+conn.execute('''CREATE TABLE categories
+		(categoryId INTEGER PRIMARY KEY,
+		name TEXT
+		)''')
+
+conn.execute('''CREATE TABLE orders
+		(orderId INTEGER PRIMARY KEY,
+        userId INTEGER,
+		productId INTEGER,
+        amount INTEGER,
+        created_at TEXT,
+        delivery_date TEXT,
+        payment_status TEXT,
+		FOREIGN KEY(userId) REFERENCES users(userId),
+		FOREIGN KEY(productId) REFERENCES products(productId)
+		)''')
+
+conn.close()
+
